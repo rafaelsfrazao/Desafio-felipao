df144caef69aed3c23729f31d6439e6f540d2c86
Rafael Frazao <rafael_b9@hotmail.com>
2024-05-31 08:00:42 -0300

    Criando uma classificação heroi

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-teste
+// variavel para guardar nome dos herois
+let nomeHeroi = ["Brian", "Diana," ,"Ektor"];
+//Variavel para guardar o elo e o xp.
+ let elo =[
+    ["Ferro", 1000 ],
+    ["Bronze", 1001, 2000],
+    ["Prata", 2001, 5000],
+    ["Ouro", 5001, 7000],
+    ["Platina", 7001, 8000],
+    ["Ascendente", 8001, 9000],
+    ["Imortal", 9001, 10000],
+    ["Radiante", 10001]
+ ]
+
+//Ainda aprendendo a construir repetições e condições.
+for     
+   
+
+
+ 
+
+
+
+console.log(heroi);
